07981a1362e4abe620266e4ce36fceca7ed1cb46
Andy Bulka <abulka@gmail.com>
2014-03-04 00:02:05 +1100

    REFORMAT main files

--- js/layout.js
+++ js/layout.js
@@ -1,9 +1,8 @@
-
 var $root_dom = $('#col_layouts');  // wish Angular could detect this and pass it in
 
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp', []);
 
-myApp.controller('ViewOptionsController', function($scope, $rootScope, layout_mode_mgr, column_config) {
+myApp.controller('ViewOptionsController', function ($scope, $rootScope, layout_mode_mgr, column_config) {
 
     $scope.show_debug = false;
     $scope.tape_mode = false;   // true, false
@@ -14,7 +13,7 @@ myApp.controller('ViewOptionsController', function($scope, $rootScope, layout_mo
 
     // Convenience method to set both vars at the same time.  An alternative is to set both
     // properties individually.
-    $scope.go = function(layout_mode, num_cols) {
+    $scope.go = function (layout_mode, num_cols) {
         //console.log('go called, vars set to: layout_mode=', layout_mode, 'num_cols=', num_cols);
         $scope.viewoptions.layout_mode = layout_mode;
         $scope.viewoptions.num_cols = num_cols;
@@ -22,12 +21,12 @@ myApp.controller('ViewOptionsController', function($scope, $rootScope, layout_mo
 
     // When watching the parent viewoptions I can change two model states at once
     // and only get one watch event - good.  The true parameter means watch recursively inside the viewoptions model
-    $scope.$watch('viewoptions', function() {
+    $scope.$watch('viewoptions', function () {
         //console.log('$watch viewoptions');
         layout_mode_mgr.achieve_layout($scope.viewoptions.num_cols, $scope.viewoptions.layout_mode);
     }, true);
 
-    $scope.$watch('tape_mode', function() {
+    $scope.$watch('tape_mode', function () {
         //console.log('angular watch tape');
         layout_mode_mgr.achieve_tape($scope.tape_mode);
     }, false);
@@ -35,29 +34,29 @@ myApp.controller('ViewOptionsController', function($scope, $rootScope, layout_mo
     // Called by the resize event and also from the dom loaded event
     // Important: In those cases need to wrap those calls within an $apply() to get angular
     // to recheck bindings etc since we are calling from a different 'turn' - see http://jimhoskins.com/2012/12/17/angularjs-and-apply.html
-    $scope.on_resize = function(calcwidth) {
+    $scope.on_resize = function (calcwidth) {
         //console.log('on_resize: width', calcwidth, ' currently layout_mode=', layout_mode, 'num_cols=', num_cols);
         //console.log("$(document).width()", $(document).width());
 
-        for (var i=0; i < column_config.trigger_widths().length; i++) {
+        for (var i = 0; i < column_config.trigger_widths().length; i++) {
             var c = column_config.trigger_widths()[i];
             if (calcwidth >= c.from
-             && calcwidth <= c.to
-             && $scope.viewoptions.num_cols != c.num_cols_should_be) {
+                && calcwidth <= c.to
+                && $scope.viewoptions.num_cols != c.num_cols_should_be) {
                 $scope.go($scope.viewoptions.layout_mode, c.num_cols_should_be);
                 break;
             }
         }
     }
 
-    $scope.show_all_layouts = function() {
+    $scope.show_all_layouts = function () {
         layout_mode_mgr.show_all_debug();
 
         // Exercise calling some services - for no reason
         $rootScope.$broadcast('handleBroadcast', "show_all_debug happening");
     }
 
-    $scope.$on('handleBroadcast', function(event, info) {
+    $scope.$on('handleBroadcast', function (event, info) {
         //console.log('EVENT: got message', info);
     });
 
@@ -68,7 +67,7 @@ myApp.controller('ViewOptionsController', function($scope, $rootScope, layout_mo
 // And you don't have to create an instance of them, which is a bonus - an instance is
 // auto injected when you pass in the name of the factory to the parameter list of the
 // angular controller.
-myApp.factory('layout_mode_mgr', function() {
+myApp.factory('layout_mode_mgr', function () {
 
     function move_widgets_in($target) {
         $target.find('.stack').html($('#stack'));
@@ -117,27 +116,27 @@ myApp.factory('layout_mode_mgr', function() {
     }
 
     return {
-        move_widgets_in:move_widgets_in,
-        move_widgets_out:move_widgets_out,
-        achieve_layout:achieve_layout,
-        achieve_tape:achieve_tape,
-        show_all_debug:show_all_debug
+        move_widgets_in: move_widgets_in,
+        move_widgets_out: move_widgets_out,
+        achieve_layout: achieve_layout,
+        achieve_tape: achieve_tape,
+        show_all_debug: show_all_debug
     }
 });
 
-myApp.factory('column_config', function() {
+myApp.factory('column_config', function () {
     return {
-        trigger_widths: function() {
-           return [
-              { from: 0, to: 450, num_cols_should_be: 1 },
-              { from: 451, to: 750, num_cols_should_be: 2 },
-              { from: 751, to: 9999, num_cols_should_be: 3 }
-           ];
+        trigger_widths: function () {
+            return [
+                { from: 0, to: 450, num_cols_should_be: 1 },
+                { from: 451, to: 750, num_cols_should_be: 2 },
+                { from: 751, to: 9999, num_cols_should_be: 3 }
+            ];
         }
-     }
- });
+    }
+});
 
-$(window).resize(function(){
+$(window).resize(function () {
     onResize();
 });
 
