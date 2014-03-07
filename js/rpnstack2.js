80bef64c087490becf28ec37d4563e571c40f3bc
Andy Bulka <abulka@gmail.com>
2014-03-08 01:09:57 +1100

    got rdn rup working

--- js/rpnstack2.js
+++ js/rpnstack2.js
@@ -16,7 +16,7 @@ myApp.controller('RpnStackController', function ($scope, $rootScope) {
         $scope.push(Math.random());
     }
     $scope.drop = function () {
-        $scope.stack.pop(0);
+        $scope.stack.pop();
     }
     $scope.dup = function () {
         if ($scope.stack.length == 0)
@@ -29,13 +29,27 @@ myApp.controller('RpnStackController', function ($scope, $rootScope) {
         var list = $scope.stack;
         if (list.length < 2)
             return;
-        var bottom_val = list.pop(0).val;
+        var bottom_val = list.pop().val;
         var second_bottom_val = list.pop(0).val;
         $scope.push(bottom_val);
         $scope.push(second_bottom_val);
         //this.scroll_to_bottom();
     },
-    
+    $scope.rdn = function () {
+        var list = $scope.stack;
+        if (list.length < 2)
+            return;
+        var bottom_val = list.pop().val;
+        list.splice(0, 0, {'val': bottom_val, 'type': typeof bottom_val});
+    },
+    $scope.rup = function () {
+        var list = $scope.stack;
+        if (list.length < 2)
+            return;
+        var top_val = list.shift().val;
+        $scope.push(top_val);
+    },
+
     // init some dummy values
     $scope.push([1, 2, 3, 4, "hi"]);
 });
