945367f79a7c2853da21aa15b28be1bd65ffcc53
Andy Bulka <abulka@gmail.com>
2014-03-08 01:37:11 +1100

    scrollable stack added

--- js/rpnstack2.js
+++ js/rpnstack2.js
@@ -3,17 +3,13 @@
  */
 
 myApp.controller('RpnStackController', function ($scope, $rootScope) {
-
-    $scope.stack = [
-        {'val': 565, 'type': 'int'},
-        {'val': 333, 'type': 'int'},
-        {'val': 'qwwewerty', 'type': 'string'}
-    ];
+    $scope.stack = [];
     $scope.push = function (val) {
         $scope.stack.push({'val': val, 'type': typeof val});
     }
     $scope.enter = function () {
         $scope.push(Math.random());
+        $scope.scroll_to_bottom();
     }
     $scope.drop = function () {
         $scope.stack.pop();
@@ -23,7 +19,7 @@ myApp.controller('RpnStackController', function ($scope, $rootScope) {
             return;
         var oldObject = $scope.stack[$scope.stack.length - 1];
         $scope.push(oldObject.val);
-        //this.scroll_to_bottom();
+        $scope.scroll_to_bottom();
     }
     $scope.swap = function () {
         var list = $scope.stack;
@@ -33,7 +29,7 @@ myApp.controller('RpnStackController', function ($scope, $rootScope) {
         var second_bottom_val = list.pop(0).val;
         $scope.push(bottom_val);
         $scope.push(second_bottom_val);
-        //this.scroll_to_bottom();
+        $scope.scroll_to_bottom();
     },
     $scope.rdn = function () {
         var list = $scope.stack;
@@ -49,8 +45,20 @@ myApp.controller('RpnStackController', function ($scope, $rootScope) {
         var top_val = list.shift().val;
         $scope.push(top_val);
     },
+    $scope.scroll_to_bottom = function() {
+        setTimeout(function() {
+            var $stackgui = $('#rpnstack2');
+            $stackgui.scrollTop(
+              $stackgui[0].scrollHeight - $stackgui.height()
+            );
+        }, 0);
+    },
 
     // init some dummy values
+    $scope.push(100);
+    $scope.push("welcome to rpn calc");
+    $scope.push({1:'a', 2:'b'});
     $scope.push([1, 2, 3, 4, "hi"]);
+    $scope.scroll_to_bottom();
 });
 
