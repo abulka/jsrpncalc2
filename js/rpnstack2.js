e133401cf41890f40cb795320bfc329456e4f15b
Andy Bulka <abulka@gmail.com>
2014-03-08 00:45:37 +1100

    got dup working

--- js/rpnstack2.js
+++ js/rpnstack2.js
@@ -4,12 +4,12 @@
 
 myApp.controller('RpnStackController', function ($scope, $rootScope) {
 
-    $scope.stack = [ {'name':565, 'type':'int'},
-                {'name':201, 'type':'int'},
-                {'name':333, 'type':'int'},
-                {'name':'qwwewerty', 'type':'string'}];
+    $scope.stack = [
+                {'val':565, 'type':'int'},
+                {'val':333, 'type':'int'},
+                {'val':'qwwewerty', 'type':'string'}];
     $scope.push = function(val) {
-        $scope.stack.push({'name':val, 'type': typeof val});
+        $scope.stack.push({'val':val, 'type': typeof val});
     }
     $scope.enter = function() {
         $scope.push(Math.random());
@@ -17,6 +17,14 @@ myApp.controller('RpnStackController', function ($scope, $rootScope) {
     $scope.drop = function() {
         $scope.stack.pop(0);
     }
+    $scope.dup = function () {
+      if ($scope.stack.length == 0)
+        return;
+      var oldObject = $scope.stack[$scope.stack.length-1];
+      $scope.push(oldObject.val);
+      //this.scroll_to_bottom();
+    }
 
+    $scope.push([1,2,3,4,"hi"]);
 });
 
